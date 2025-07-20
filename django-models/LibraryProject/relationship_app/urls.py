a5955a3247b0f57db99c7f2d9938147ec5e8c44d
wamamili <wamamiliclean@gmail.com>
2025-07-20 23:22:38 +0300

    Django Models and Views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,23 +1,31 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-
-from relationship_app.views import admin_view, librarian_view, member_view
-from . import views
+from .views import (
+    list_books,
+    add_book,
+    edit_book,
+    delete_book,
+    LibraryDetailView,
+    register_view,
+    login_view,
+    logout_view,
+    admin_dashboard,
+    librarian_dashboard,
+    member_dashboard
+)
 
 urlpatterns = [
-    path('admin-area/', admin_view.admin_dashboard, name='admin_view'),
-    path('librarian-area/', librarian_view.librarian_dashboard, name='librarian_view'),
-    path('member-area/', views.member_dashboard, name='member_view'),
-    path('register/', views.register_view, name='register'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
-    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
-    
-    
-    # Authentication URLs
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', list_books, name='list_books'),
+    path('books/add/', add_book, name='add_book'),
+    path('books/edit/<int:pk>/', edit_book, name='edit_book'),
+    path('books/delete/<int:pk>/', delete_book, name='delete_book'),
+
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    path('dashboard/admin/', admin_dashboard, name='admin_dashboard'),
+    path('dashboard/librarian/', librarian_dashboard, name='librarian_dashboard'),
+    path('dashboard/member/', member_dashboard, name='member_dashboard'),
 ]
