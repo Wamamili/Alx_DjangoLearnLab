b7fe017e01a41740295069c40452796caeb6e712
wamamili <wamamiliclean@gmail.com>
2025-07-20 23:39:33 +0300

    Django Models and Views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('books/add/', views.add_book, name='add_book'),
     path('edit_book/', views.edit_book, name='edit_book'),
+    path('add_book/', views.add_book, name='add_book'),
     path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
     path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
     
