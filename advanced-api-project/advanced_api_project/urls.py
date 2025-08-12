d509717c515fe70662af0784226a2e1c981ebed3
wamamili <wamamiliclean@gmail.com>
2025-08-12 20:53:13 +0300

    Building a Complete Django Application

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    
 ]
 # This file defines the URL routing for the Django project.
 # It includes the admin interface and the API URLs from the 'api' app.
