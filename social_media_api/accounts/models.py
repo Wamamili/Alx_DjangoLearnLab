2f050231563b25350ba504abc106b4598ffe887d
wamamili <wamamiliclean@gmail.com>
2025-08-22 14:47:03 +0300

    Building and deploying a Django API

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -7,7 +7,46 @@ from django.db import models
 class User(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
-    followers = models.ManyToManyField("self", symmetrical=False, related_name="following", blank=True)
+    
+    followers = models.ManyToManyField(
+        "self",
+        through="UserFollower",
+        through_fields=("user", "follower"),
+        symmetrical=False,
+        related_name="following",
+        blank=True
+    )
+
+    def follow(self, user):
+        if user != self:
+            UserFollower.objects.get_or_create(user=user, follower=self)
+
+    def unfollow(self, user):
+        if user != self:
+            UserFollower.objects.filter(user=user, follower=self).delete()
+
+    def is_following(self, user):
+        return UserFollower.objects.filter(user=user, follower=self).exists()
 
     def __str__(self):
         return self.username
+
+
+class UserFollower(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name="user_followers",
+        on_delete=models.CASCADE
+    )
+    follower = models.ForeignKey(
+        User,
+        related_name="user_following",
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "follower") 
+
+    def __str__(self):
+        return f"{self.follower} follows {self.user}"
