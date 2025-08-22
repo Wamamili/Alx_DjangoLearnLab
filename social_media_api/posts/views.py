0bc722f0b21f8da7af37ca5c8450b1158cc5470f
wamamili <wamamiliclean@gmail.com>
2025-08-22 15:31:13 +0300

    Building and deploying a Django API

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, permissions, filters
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from rest_framework import generics, permissions
+from rest_framework.response import Response
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -40,4 +41,19 @@ class FeedView(generics.ListAPIView):
         # Posts from people the user follows + their own posts
         following_ids = user.followers.values_list("id", flat=True)
         return Post.objects.filter(author__id__in=following_ids).order_by("-created_at")
+    
+class FeedView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = PostSerializer
+
+    def get(self, request):
+        # Get all users the current user follows
+        following_users = request.user.following.all()
+
+        # Query posts from those users
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+
+        serializer = self.get_serializer(posts, many=True)
+        return Response(serializer.data)
+
 
