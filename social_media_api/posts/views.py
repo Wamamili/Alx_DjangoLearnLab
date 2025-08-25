09cc8059b25f413518693b6bae8218b01615ed90
wamamili <wamamiliclean@gmail.com>
2025-08-25 10:52:51 +0300

    Building and deploying a Django API

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -10,7 +10,6 @@ from django.shortcuts import get_object_or_404
 from notifications.models import Notification
 from django.contrib.contenttypes.models import ContentType
 
-
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Allow read for anyone, write only for owner
@@ -67,13 +66,12 @@ class LikePostView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
 
         if not created:
             return Response({"detail": "You already liked this post"}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Create notification
         if post.author != request.user:  # don't notify yourself
             Notification.objects.create(
                 recipient=post.author,
@@ -88,7 +86,7 @@ class UnlikePostView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like = Like.objects.filter(user=request.user, post=post)
 
         if like.exists():
